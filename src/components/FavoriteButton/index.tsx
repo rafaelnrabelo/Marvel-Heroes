499c8d2e45d74726c1ff908a1423ae32c7b4cc39
Rafael Nogueira Rabelo <rafaelnrabelo@gmail.com>
2022-07-02 22:54:45 -0300

    refactor: modularize favorite button

---
+++ src/components/FavoriteButton/index.tsx
@@ -0,0 +1,30 @@
+import { useHeroes } from "../../contexts/HeroesContext";
+import styles from "../../styles/FavoriteButton.module.scss";
+
+interface FavoriteButtonProps {
+  id?: number;
+  size?: number;
+}
+
+const FavoriteButton: React.FC<FavoriteButtonProps> = ({
+  id = 0,
+  size = 20,
+}) => {
+  const { toggleFavoriteHero, isFavorite } = useHeroes();
+  const favorite = isFavorite(id);
+
+  return (
+    <button
+      className={styles.favorite_button}
+      onClick={() => toggleFavoriteHero(id)}
+    >
+      <img
+        src={favorite ? "/assets/favorito_01.svg" : "/assets/favorito_02.svg"}
+        width={size}
+        height={size}
+      />
+    </button>
+  );
+};
+
+export default FavoriteButton;
